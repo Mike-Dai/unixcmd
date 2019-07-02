2e5a277b2ebe96f201959dbe96291a4b4b6303e8
Mike-Dai <mrmikedai@outlook.com>
2019-07-02 11:21:43 +0800

    Add logout in who, fail

--- who1.c
+++ who1.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define SHOWHOST
 
@@ -48,4 +49,38 @@ void showtime(long timeval) {
 	char* cp;
 	cp = ctime(&timeval);
 	printf("%12.12s", cp+4);
+}
+
+/* marks a utmp record as logged out
+does not blank username or remote host
+return -1 on error, 0 on success
+*/
+int logout_tty(char* line) {
+	int fd;
+	struct utmp rec;
+	int len = sizeof(struct utmp);
+	int retval = -1;
+
+	if ((fd = open(UTMP_FILE, O_RDWR)) == -1) {
+		return -1;
+	}
+	/* search and replace */
+	while (read(fd, &rec, len) == len) {
+		if (strncmp(rec.ut_line, line, sizeof(rec.ut_line)) == 0) {
+			rec.ut_type = DEAD_PROCESS;                  /* set type */
+			if (time(&rec.ut_time) != -1) {  //???????
+				if (lseek(fd, -len, SEEK_CUR) != -1) {   /* back up */
+					if (write(fd, &rec, len) == len) {   /* update */
+						retval = 0;                      /* success */
+					}
+				}
+			}
+			break;  
+		}
+	}
+	/* close the file */
+	if (close(fd) == -1) {
+		retval = -1;
+	}
+	return retval;
 }
