9a6d3e60cc7cd7a8e3dcebbfc66fc9d94a058fd9
Mike-Dai <mrmikedai@outlook.com>
2019-07-09 20:15:50 +0800

    Write a ticker demo using SIGALRM

--- echostate.c
+++ echostate.c
@@ -1,10 +1,14 @@
+/* echostate.c
+ * reports current state of echo bit in tty driver for fd 0
+ * shows how to read attributes from driver and test a bit
+ */
 #include <stdio.h>
 #include <termios.h>
 #include <stdlib.h>
 int main() {
 	struct termios info;
 	int rv;
-	rv = tcgetattr(0, &info);
+	rv = tcgetattr(0, &info); /* read values from driver */
 	if (rv == -1) {
 		perror("tcgetattr");
 		exit(1);
